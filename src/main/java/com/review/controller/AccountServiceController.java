5a8c6ceb4753f0189ecce97a299f338ed831b513
Gokay <gokayyazar@hotmail.com>
2023-10-07 21:16:43 -0400

    Create Account implemented and refactor create.html

--- src/main/java/com/review/controller/AccountServiceController.java
+++ src/main/java/com/review/controller/AccountServiceController.java
@@ -1,13 +1,18 @@
 package com.review.controller;
 
+import com.review.enums.AccountType;
+import com.review.model.Account;
 import com.review.service.AccountService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Date;
 
 @Controller
-public class AccountServiceController
-{
+public class AccountServiceController {
 
     private final AccountService accountService;
 
@@ -16,9 +21,29 @@ public class AccountServiceController
     }
 
     @GetMapping("index")
-    public String getIndexPage(Model model){
+    public String getIndexPage(Model model) {
 
         model.addAttribute("accountList", accountService.listAllAccount());
         return "account/index";
+    }
+
+    @GetMapping("/create-form")
+    public String getCreateForm(Model model) {
+
+        //we need to provide empty account object
+        model.addAttribute("account", Account.builder().build());
+        //we need to provide accountType enum info for filling the dropdown options
+        model.addAttribute("accountTypes",AccountType.values());
+
+        return "account/create-account";
+    }
+
+    @PostMapping("/create")
+    public String createNewAccount(@ModelAttribute("account") Account account, Model model){
+
+        System.out.println(account);
+        accountService.createNewAccount(account.getBalance(), new Date(), account.getAccountType(), account.getUserId());
+
+        return "redirect:/index";
     }
 }
